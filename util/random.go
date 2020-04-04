d0f1e066e163aec8786735b6091736fa67960f50
Ruggero Cucchiani <weregoat.github@maialinux.org>
2020-04-04 11:38:18 +0200

    use crypto/rand for random padding

--- util/random.go
+++ util/random.go
@@ -1,9 +1,10 @@
 package util
 
 import (
-	cryptorand "crypto/rand"
+	rand "crypto/rand"
 	"log"
-	"math/rand"
+	"math/big"
+
 )
 
 // GenerateRandomKey returns a slice of random bytes of the given length.
@@ -13,17 +14,31 @@ func RandomBytes(size int) ([]byte, error) {
 		return key, nil
 	}
 	key = make([]byte, size)
-	_,err := cryptorand.Read(key)
+	_,err := rand.Read(key)
 	return key, err
 }
 
-// Not cryptographically secure
+func RandomInt(min, max int) int {
+	a := int64(min)
+	b := int64(max)
+	diff := big.NewInt(b - a)
+	r, err := rand.Int(rand.Reader, diff)
+	if err != nil {
+		// The RNG must have failed
+		log.Fatal(err)
+	}
+	c := int(r.Int64())+min
+	return c
+}
+
+// RandomPad returns a byte slice padded at both ends with a random number of
+// bytes picked between min and max each time.
 func RandomPad(src []byte, min,max int) []byte {
-	before, err := RandomBytes(rand.Intn(max-min)+min)
+	before, err := RandomBytes(RandomInt(min,max))
 	if err != nil {
 		log.Fatal(err)
 	}
-	after, err := RandomBytes(rand.Intn(max-min)+min)
+	after, err := RandomBytes(RandomInt(min,max))
 	if err != nil {
 		log.Fatal(err)
 	}
