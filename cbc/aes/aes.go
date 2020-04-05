e175f004b37a821265a8de134facca088cc91d5d
Ruggero Cucchiani <weregoat.github@maialinux.org>
2020-04-05 09:18:45 +0200

    update CBC code
    Updated the CBC code not to manipulate the splices directly.
    I found this way to implement it clearer in showing how CBC
    works.
    It's probably slower, but, right now performance is not an issue.
    Less hard-core too, I suppose, but that's also not an issue; clear
    over clever and all that...

--- cbc/aes/aes.go
+++ cbc/aes/aes.go
@@ -11,66 +11,79 @@ const BlockSize = aes.BlockSize
 
 // Encrypt returns a ciphertext encrypted with AES in CBC mode.
 func Encrypt(plainText, key, iv []byte) ([]byte, error) {
-	IV := make([]byte, len(iv)) // Need to make a copy
+	var cipherText []byte
+	// We need to make a copy of the IV because otherwise we'll change it
+	IV := make([]byte, len(iv))
 	copy(IV, iv)
-	if len(plainText)%BlockSize != 0 {
-		plainText = pkcs7.Pad(plainText, BlockSize)
-	}
-	cipherText := make([]byte, len(plainText))
+	// Pad, if needed, and split the plaintext into blocks
+	blocks := pkcs7.Split(plainText, BlockSize)
+	// Initialise the AES cipher block
 	cipher, err := aes.NewCipher(key)
 	if err != nil {
 		return cipherText, err
 	}
-	i := 0
-	for {
-		bs := i * BlockSize
-		if bs >= len(cipherText) {
-			break
-		}
-		be := (i + 1) * BlockSize
-		xor, err := util.FixedXORBytes(plainText[bs:be], IV)
+	// Encrypt each block
+	// By using copies we make sure we don't mess with original slices
+	// It's, probably, slower than manipulating the slices directly, but
+	// I find it much clearer to show how CBC works.
+	for _,block := range blocks {
+		// Copy each block so we can manipulate it
+		src := make([]byte, BlockSize)
+		copy(src,block)
+		// XOR the block with the IV
+		xor, err := util.FixedXORBytes(src, IV)
 		if err != nil {
 			return cipherText, err
 		}
-		cipher.Encrypt(cipherText[bs:be], xor)
-		i++
-		copy(IV, cipherText[bs:be])
+		// Create an empty block for the plaintext
+		dst := make([]byte, BlockSize)
+		// Encrypt the XOR block into the plaintext block
+		cipher.Encrypt(dst, xor)
+		cipherText = append(cipherText, dst...)
+		copy(IV, dst)
 	}
-
 	return cipherText, nil
 }
 
 // Decrypt returns a plaintext from a ciphertext encrypted with AES in CBC mode.
 func Decrypt(cipherText, key, iv []byte) ([]byte, error) {
+	var plainText []byte
+	// Make a copy of the IV to avoid changing it
 	IV := make([]byte, len(iv))
 	copy(IV, iv)
-	var plainText []byte
+	// The cipherText length should be a multiple of AES blocksize (128 bits/16 bytes)
 	if len(cipherText)%BlockSize != 0 {
 		err := fmt.Errorf("ciphertext byte length(%d) must be a multiple of %d bytes", len(cipherText), BlockSize)
 		return plainText, err
 	}
-	plainText = make([]byte, len(cipherText))
+	// Initialise the AES cipher
 	cipher, err := aes.NewCipher(key)
 	if err != nil {
 		return plainText, err
 	}
-	i := 0
-	for {
-		bs := i * BlockSize
-		if bs >= len(cipherText) {
-			break
-		}
-		be := (i + 1) * BlockSize
-		decripted := make([]byte, BlockSize)
-		cipher.Decrypt(decripted, cipherText[bs:be])
-		xor, err := util.FixedXORBytes(decripted, IV)
+	// We should not need padding.
+	// PKCS7 split function would work as well, but here
+	// using this for
+	blocks := util.Split(cipherText, BlockSize)
+	// Go through each block and decipher it.
+	for _,block := range blocks {
+		// Copy the block, so we don't change it
+		src := make([]byte, BlockSize)
+		copy(src, block)
+		// Create a block to store the deciphered block into
+		dst := make([]byte, BlockSize)
+		// Decrypt the block of ciphertext
+		cipher.Decrypt(dst, src)
+		// XOR the decrypted block with the IV
+		xor, err := util.FixedXORBytes(dst, IV)
 		if err != nil {
 			return plainText, err
 		}
-		i++
-		copy(plainText[bs:be], xor)
-		copy(IV, cipherText[bs:be])
-
+		// Add the resulting block to the plaintext
+		plainText = append(plainText, xor...)
+		// Update the IV with the original cipher block
+		copy(IV, src)
 	}
+	// Remove the padding before returning
 	return pkcs7.RemovePadding(plainText), nil
 }
